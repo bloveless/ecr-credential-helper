95dc3da5d9deda2431b6655c0f908adc506b4ebd
Brennon Loveless <brennon.loveless@gmail.com>
2019-12-21 09:30:11 -0500

    Able to connect to kubernetes and aws ecr. Release binary is 9.4 MB

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-extern crate dotenv;
-
 use dotenv::dotenv;
 use std::env;
+use std::error::Error;
 use rusoto_core::Region;
-use rusoto_ecr::{
-    EcrClient,
-    Ecr,
-    GetAuthorizationTokenRequest
-};
+use rusoto_ecr::{EcrClient, Ecr, GetAuthorizationTokenRequest};
 
 use kube::{
     api::Api,
@@ -15,45 +10,29 @@ use kube::{
     config,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     std::env::set_var("RUST_LOG", "info,kube=trace");
 
-    let config = config::load_kube_config().expect("failed to load kubeconfig");
+    let config = config::load_kube_config().await?;
     let client = APIClient::new(config);
 
     // Manage pods
     let pods = Api::v1Pod(client).within("fritzandandre");
 
-    let fna = pods.get("fritzandandre-php-0").expect("Failed to get fna pod");
+    let fna = pods.get("fritzandandre-php-0").await?;
+
     println!("Got blog pod with containers: {:?}", fna.spec.containers);
 
-    let aws_account_id = env::var("AWS_ACCOUNT_ID").unwrap_or_default();
-    let aws_secret_access_key = env::var("AWS_SECRET_ACCESS_KEY").unwrap_or_default();
-    let aws_access_key_id = env::var("AWS_ACCESS_KEY_ID").unwrap_or_default();
-    let aws_default_region = env::var("AWS_DEFAULT_REGION").unwrap_or_default();
-    let email = env::var("EMAIL").unwrap_or_default();
-    let namespaces = env::var("NAMESPACES").unwrap_or_default();
+    let aws_account_id = env::var("AWS_ACCOUNT_ID").unwrap();
+    let aws_secret_access_key = env::var("AWS_SECRET_ACCESS_KEY").unwrap();
+    let aws_access_key_id = env::var("AWS_ACCESS_KEY_ID").unwrap();
+    let aws_default_region = env::var("AWS_DEFAULT_REGION").unwrap();
+    let email = env::var("EMAIL").unwrap();
+    let namespaces = env::var("NAMESPACES").unwrap();
 
-    let client = EcrClient::new(Region::UsWest2);
-    let mut registry_ids: Vec<String> = Vec::new();
-    registry_ids.push(aws_account_id.to_string());
-
-    let token_request = GetAuthorizationTokenRequest { registry_ids: Some(registry_ids) };
-    let token = match client.get_authorization_token(token_request).sync() {
-        Ok(token) => token,
-        Err(e) => panic!("e: {}", e),
-    };
-
-    let data = token.authorization_data.unwrap();
-    let data = data.first().unwrap();
-
-    let token = match data.authorization_token {
-        Some(token) => token,
-        None => 0,
-    };
-
-    println!("data {:?}", data.authorization_token);
+    let authorization_token = get_aws_authorization_token(Region::UsWest2, &aws_account_id)?;
 
     println!(
         "AWS_ACCOUNT_ID: {}\nAWS_SECRET_ACCESS_KEY: {}\nAWS_ACCESS_KEY_ID: {}\nAWS_DEFAULT_REGION: {}\nEMAIL: {}\nNAMESPACES: {}\n",
@@ -65,5 +44,26 @@ fn main() {
         namespaces
     );
 
-    println!("Hello, world!");
+    println!("AUTHORIZATION TOKEN: {}\n", authorization_token);
+
+    Ok(())
+}
+
+fn get_aws_authorization_token(region: Region, aws_account_id: &String) -> Result<String, Box<dyn Error>> {
+    let client = EcrClient::new(region);
+    let token_request = GetAuthorizationTokenRequest {
+        registry_ids: Some(vec![aws_account_id.to_string()])
+    };
+
+    let auth_token_response = client
+        .get_authorization_token(token_request)
+        .sync()?;
+
+    let auth_data = auth_token_response
+        .authorization_data
+        .ok_or("Unable to get authorization data")?;
+
+    let token = auth_data.first().ok_or("Response didn't contain a token")?;
+
+    Ok(token.authorization_token.clone().unwrap())
 }
